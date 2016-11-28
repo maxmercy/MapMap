9f07de9e34aaf9758eab4cc1bc2e7073e821eed7
Maxime Mercy <mxmercy@gmail.com>
2016-11-28 17:22:13 +0100

    create view for user profil login logout with devise implemented

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = current_user
+  end
+
+  def index
+    @users = User.all
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+
+  get 'welcome/index'
+  root 'welcome#index'
+
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'users/', to: 'users#index'
+  get 'users/:user_id', to: 'users#show', as: :profil_user
+
 end
