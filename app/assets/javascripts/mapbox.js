5a8f8a09527777f5a3be34986ed717d67bdd06aa
Maxime Mercy <mxmercy@gmail.com>
2016-11-28 18:16:22 +0100

    mapbox added to the user show with JS

---
+++ app/assets/javascripts/mapbox.js
@@ -0,0 +1,16 @@
+
+// personal access token will have to be hidden
+
+$(document).ready(function() {
+
+    initMap();
+});
+
+
+function initMap() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibWF4bWVyY3kiLCJhIjoiY2l3MXplMTRwMDhibDJ0cGNrY2QzbmZoayJ9.Ybq0sAYRjcEwrjeoMbP0Pg';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9'
+    });
+};
