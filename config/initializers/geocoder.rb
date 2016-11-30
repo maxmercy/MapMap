84683a6e38a9e9c6ec70257b9d707e5027c6f133
Maxime Mercy <mxmercy@gmail.com>
2016-11-30 09:29:37 +0100

    mapbox

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,12 +1,12 @@
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
-  # lookup: :mapbox,            # name of geocoding service (symbol)
+   lookup: :mapbox,            # name of geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  # api_key: 'pk.eyJ1IjoibWF4bWVyY3kiLCJhIjoiY2l3MXplMTRwMDhibDJ0cGNrY2QzbmZoayJ9.Ybq0sAYRjcEwrjeoMbP0Pg',           # API key for geocoding service
+   api_key: 'pk.eyJ1IjoibWF4bWVyY3kiLCJhIjoiY2l3MXplMTRwMDhibDJ0cGNrY2QzbmZoayJ9.Ybq0sAYRjcEwrjeoMbP0Pg',           # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #keys)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
@@ -16,6 +16,6 @@ Geocoder.configure(
   # always_raise: [],
 
   # Calculation options
-   # units: :km,                 # :km for kilometers or :mi for miles
+   units: :km,                 # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
 )
