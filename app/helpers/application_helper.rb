65eacb5fdbeb6e6164a162d4b8e2404277582be3
Maxime Mercy <mxmercy@gmail.com>
2016-12-08 20:00:42 +0100

    modal to login/signup from index and how_public

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 module ApplicationHelper
 
+
+# For login from anywhere in the app
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+   @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
