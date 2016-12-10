5309be44e491a091d6c222aa3ab26d82f0b2d3b4
Maxime Mercy <mxmercy@gmail.com>
2016-12-10 14:16:58 +0100

    prepare for Heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161209105846) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -27,9 +30,9 @@ ActiveRecord::Schema.define(version: 20161209105846) do
     t.datetime "updated_at",                 null: false
     t.integer  "category_id",   default: 1
     t.integer  "sprite_number", default: 16
-    t.index ["category_id"], name: "index_map_places_on_category_id"
-    t.index ["map_id"], name: "index_map_places_on_map_id"
-    t.index ["place_id"], name: "index_map_places_on_place_id"
+    t.index ["category_id"], name: "index_map_places_on_category_id", using: :btree
+    t.index ["map_id"], name: "index_map_places_on_map_id", using: :btree
+    t.index ["place_id"], name: "index_map_places_on_place_id", using: :btree
   end
 
   create_table "maps", force: :cascade do |t|
@@ -42,7 +45,7 @@ ActiveRecord::Schema.define(version: 20161209105846) do
     t.datetime "updated_at",                null: false
     t.string   "city"
     t.string   "public_id"
-    t.index ["user_id"], name: "index_maps_on_user_id"
+    t.index ["user_id"], name: "index_maps_on_user_id", using: :btree
   end
 
   create_table "places", force: :cascade do |t|
@@ -70,8 +73,9 @@ ActiveRecord::Schema.define(version: 20161209105846) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "maps", "users"
 end
