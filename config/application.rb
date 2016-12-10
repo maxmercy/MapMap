46cf501339c669f3c54886f54acfe4862cf03816
Maxime Mercy <mxmercy@gmail.com>
2016-12-10 15:02:18 +0100

    add api key for geocoder

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module MapMap
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.initialize_on_precompile = false
 
   end
 end
