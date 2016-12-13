eb492c82c9e8259f557577928162839e5ba94175
Maxime Mercy <mxmercy@gmail.com>
2016-12-13 19:24:25 +0100

    correction error in mailer config smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,14 +57,15 @@ Rails.application.configure do
 
 
   config.action_mailer.default_url_options = { :host => 'maxmap.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "maxmap.herokuapp.com",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
