9be30bd2d079e55a8d276058df52cf6bdeac665a
Maxime Mercy <mxmercy@gmail.com>
2016-12-14 09:26:26 +0100

    new favicon

--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,4 @@ Rails.application.routes.draw do
   post '/maps/:id/share', to: 'maps#map_share', as: :map_share
   get '/maps/s/:public_id', to: 'maps#show_public', as: :map_public
   post '/maps/s/:public_id', to: 'maps#save_public', as: :save_map_public
-  # post  '/maps/:id/duplicate', to: 'maps#duplicate', as: :duplicate_map
-  # get '/map_places/:id', to: 'map_places#show', as: :map_place_show
-  # delete '/map_places/delete' , to: 'map_places#destroy', as: :unlink_map_place
-  # patch  '/map_places/:id', to: 'map_places#update'
-
 end
