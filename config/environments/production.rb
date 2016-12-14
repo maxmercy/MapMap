1e06d9315d4a9e0b5ba28f8fa459caf1ff0d76ac
Maxime Mercy <mxmercy@gmail.com>
2016-12-14 09:35:16 +0100

    mailer again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,11 +57,11 @@ Rails.application.configure do
 
 
   config.action_mailer.default_url_options = { :host => 'maxmap.herokuapp.com' }
-
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
